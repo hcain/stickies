19b96b2754c63199c6aeda8f1b2bfcdffc3e4a7c
hcain <hannah.cain@craftww.com>
2019-06-12 15:33:50 -0400

    updated footer - mostly fixed overflow on mobile on postForm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,23 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <p>This is the footer</p>
+        <p>
+          Built by Hannah Cain. Find me on{" "}
+          <a
+            href="https://github.com/hcain"
+            target="_blank"
+            rel="noopener noreferrer">
+            Github
+          </a>{" "}
+          or{" "}
+          <a
+            href="https://www.linkedin.com/in/hannahcain"
+            target="_blank"
+            rel="noopener noreferrer">
+            LinkedIn
+          </a>
+          .
+        </p>
       </div>
     );
   }
